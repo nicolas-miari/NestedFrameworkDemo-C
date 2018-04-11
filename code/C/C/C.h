b499455c37f5f0ff50801b9fabe9642b319f7eb8
Nicolás Miari <nicolasmiari@gmail.com>
2018-04-11 20:40:17 +0900

    Add sample class

---
+++ code/C/C/C.h
@@ -0,0 +1,19 @@
+//
+//  C.h
+//  C
+//
+//  Created by Nicolás Miari on 2018/04/11.
+//  Copyright © 2018 Nicolás Miari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for C.
+FOUNDATION_EXPORT double CVersionNumber;
+
+//! Project version string for C.
+FOUNDATION_EXPORT const unsigned char CVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <C/PublicHeader.h>
+
+
---
+++ code/C/C/SampleClass.swift
@@ -0,0 +1,14 @@
+//
+//  SampleClass.swift
+//  C
+//
+//  Created by Nicolás Miari on 2018/04/11.
+//  Copyright © 2018 Nicolás Miari. All rights reserved.
+//
+
+import Foundation
+
+open class SampleClass {
+    public init() {
+    }
+}
